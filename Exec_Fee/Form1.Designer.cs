62b991d90db01acc96ebfa699c6f49753efb0a7c
xbertstorm5566 <xbertstorm5566@gmail.com>
2022-10-25 19:05:35 +0800

    加入專案檔案。

---
+++ Exec_Fee/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace Exec_Fee
+{
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.radioButton2 = new System.Windows.Forms.RadioButton();
+			this.radioButton1 = new System.Windows.Forms.RadioButton();
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.groupBox1.SuspendLayout();
+			this.groupBox2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.label7);
+			this.groupBox1.Controls.Add(this.radioButton2);
+			this.groupBox1.Controls.Add(this.radioButton1);
+			this.groupBox1.Controls.Add(this.button1);
+			this.groupBox1.Controls.Add(this.textBox1);
+			this.groupBox1.Controls.Add(this.label2);
+			this.groupBox1.Controls.Add(this.label1);
+			this.groupBox1.Font = new System.Drawing.Font("Microsoft JhengHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.groupBox1.Location = new System.Drawing.Point(12, 12);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(398, 187);
+			this.groupBox1.TabIndex = 0;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "客戶資訊";
+			// 
+			// radioButton2
+			// 
+			this.radioButton2.AutoSize = true;
+			this.radioButton2.Location = new System.Drawing.Point(310, 52);
+			this.radioButton2.Name = "radioButton2";
+			this.radioButton2.Size = new System.Drawing.Size(59, 24);
+			this.radioButton2.TabIndex = 3;
+			this.radioButton2.Text = "女性";
+			this.radioButton2.UseVisualStyleBackColor = true;
+			// 
+			// radioButton1
+			// 
+			this.radioButton1.AutoSize = true;
+			this.radioButton1.Checked = true;
+			this.radioButton1.Location = new System.Drawing.Point(248, 52);
+			this.radioButton1.Name = "radioButton1";
+			this.radioButton1.Size = new System.Drawing.Size(59, 24);
+			this.radioButton1.TabIndex = 3;
+			this.radioButton1.TabStop = true;
+			this.radioButton1.Text = "男性";
+			this.radioButton1.UseVisualStyleBackColor = true;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(15, 100);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(354, 46);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "計算";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(64, 48);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 28);
+			this.textBox1.TabIndex = 1;
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(183, 54);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(59, 24);
+			this.label2.TabIndex = 0;
+			this.label2.Text = "性別：";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(15, 51);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(57, 20);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "年齡：";
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.label6);
+			this.groupBox2.Controls.Add(this.label5);
+			this.groupBox2.Controls.Add(this.label4);
+			this.groupBox2.Controls.Add(this.label3);
+			this.groupBox2.Font = new System.Drawing.Font("Microsoft JhengHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.groupBox2.Location = new System.Drawing.Point(12, 205);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(398, 187);
+			this.groupBox2.TabIndex = 0;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "計算結果";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(136, 100);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(30, 20);
+			this.label6.TabIndex = 3;
+			this.label6.Text = "---";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(136, 54);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(30, 20);
+			this.label5.TabIndex = 2;
+			this.label5.Text = "---";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(35, 100);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(57, 20);
+			this.label4.TabIndex = 1;
+			this.label4.Text = "原因：";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(35, 52);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(57, 20);
+			this.label3.TabIndex = 0;
+			this.label3.Text = "車資：";
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Font = new System.Drawing.Font("Microsoft JhengHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.label7.Location = new System.Drawing.Point(248, 77);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(99, 20);
+			this.label7.TabIndex = 4;
+			this.label7.Text = "(預設為男性)";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(425, 402);
+			this.Controls.Add(this.groupBox2);
+			this.Controls.Add(this.groupBox1);
+			this.Font = new System.Drawing.Font("Microsoft JhengHei UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "Form1";
+			this.Text = "計算車資";
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.groupBox2.ResumeLayout(false);
+			this.groupBox2.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private GroupBox groupBox1;
+		private RadioButton radioButton2;
+		private RadioButton radioButton1;
+		private Button button1;
+		private TextBox textBox1;
+		private Label label2;
+		private Label label1;
+		private GroupBox groupBox2;
+		private Label label6;
+		private Label label5;
+		private Label label4;
+		private Label label3;
+		private Label label7;
+	}
+}
---
+++ Exec_Fee/Form1.cs
@@ -0,0 +1,121 @@
+using System.CodeDom;
+
+namespace Exec_Fee
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+			label5.Text = String.Empty;
+			label6.Text = String.Empty;
+			FormBorderStyle = FormBorderStyle.Fixed3D;
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			int number;
+			try
+			{
+				number = SetNumber();
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+				return;
+			}
+			FeeCheck(GetGender(), number);
+
+		}
+		private int SetNumber()
+		{
+			TextBox txt = textBox1;
+			string input = txt.Text;
+			return GetInt(txt, input);
+		}
+		private int GetInt(TextBox txt, string input)
+		{
+			string value = txt.Text;
+			bool Isint = int.TryParse(value, out int number);
+			return Isint ? number : throw new Exception($"{input}必須要填值");
+		}
+		private string GetGender()
+		{
+			if (radioButton1.Checked) return "Male";
+			else if (radioButton2.Checked) return "Female";
+			else throw new Exception("沒有選擇性別");
+		}
+		private string FeeCheck(string gender, int age)
+		{			
+			if (gender == "Male")
+			{
+				if (age <= 3)
+				{
+					Result("Male", age);
+					return label5.Text = "0";
+				}
+				else if (age > 3 && age < 70)
+				{
+					Result("Male", age);
+					return label5.Text = "15";
+				}
+				else
+				{
+					Result("Male", age);
+					return label5.Text = "2";
+				}
+			}
+			else
+			{
+				if (age <= 3)
+				{
+					Result("Female", age);
+					return label5.Text = "0";
+				}
+				else if (age > 3 && age < 60)
+				{
+					Result("Female", age);
+					return label5.Text = "15";
+				}
+				else
+				{
+					Result("Female", age);
+					return label5.Text = "3";
+				}
+			}
+		}
+		private string Result(string gender, int age)
+		{
+			if (gender == "Male")
+			{
+				if (age <= 3)
+				{
+					return label6.Text = "男性：未達收費年齡";
+				}
+				else if (age > 3 && age < 70)
+				{
+					return label6.Text = "男性：全票收費";
+				}
+				else
+				{
+					return label6.Text = "男性：符合敬老票年齡";
+				}
+			}
+			else
+			{
+				if (age <= 3)
+				{
+					return label6.Text = "女性：未達收費年齡";
+				}
+				else if (age > 3 && age < 60)
+				{
+					return label6.Text = "女性：全票收費";
+				}
+				else
+				{
+					return label6.Text = "女性：符合敬老票年齡";
+				}
+			}
+		}
+	}
+}
