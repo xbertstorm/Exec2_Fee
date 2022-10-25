21a4bf48e3ce952c2349ddbc050daf788a03c92a
xbertstorm5566 <xbertstorm5566@gmail.com>
2022-10-25 19:09:11 +0800

    Changed-1

--- Exec_Fee/Form1.Designer.cs
+++ Exec_Fee/Form1.Designer.cs
@@ -29,6 +29,7 @@
 		private void InitializeComponent()
 		{
 			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.label7 = new System.Windows.Forms.Label();
 			this.radioButton2 = new System.Windows.Forms.RadioButton();
 			this.radioButton1 = new System.Windows.Forms.RadioButton();
 			this.button1 = new System.Windows.Forms.Button();
@@ -40,7 +41,6 @@
 			this.label5 = new System.Windows.Forms.Label();
 			this.label4 = new System.Windows.Forms.Label();
 			this.label3 = new System.Windows.Forms.Label();
-			this.label7 = new System.Windows.Forms.Label();
 			this.groupBox1.SuspendLayout();
 			this.groupBox2.SuspendLayout();
 			this.SuspendLayout();
@@ -62,13 +62,23 @@
 			this.groupBox1.TabStop = false;
 			this.groupBox1.Text = "客戶資訊";
 			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Font = new System.Drawing.Font("Microsoft JhengHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.label7.Location = new System.Drawing.Point(248, 77);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(99, 20);
+			this.label7.TabIndex = 5;
+			this.label7.Text = "(預設為男性)";
+			// 
 			// radioButton2
 			// 
 			this.radioButton2.AutoSize = true;
 			this.radioButton2.Location = new System.Drawing.Point(310, 52);
 			this.radioButton2.Name = "radioButton2";
 			this.radioButton2.Size = new System.Drawing.Size(59, 24);
-			this.radioButton2.TabIndex = 3;
+			this.radioButton2.TabIndex = 2;
 			this.radioButton2.Text = "女性";
 			this.radioButton2.UseVisualStyleBackColor = true;
 			// 
@@ -79,7 +89,7 @@
 			this.radioButton1.Location = new System.Drawing.Point(248, 52);
 			this.radioButton1.Name = "radioButton1";
 			this.radioButton1.Size = new System.Drawing.Size(59, 24);
-			this.radioButton1.TabIndex = 3;
+			this.radioButton1.TabIndex = 1;
 			this.radioButton1.TabStop = true;
 			this.radioButton1.Text = "男性";
 			this.radioButton1.UseVisualStyleBackColor = true;
@@ -89,7 +99,7 @@
 			this.button1.Location = new System.Drawing.Point(15, 100);
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(354, 46);
-			this.button1.TabIndex = 2;
+			this.button1.TabIndex = 3;
 			this.button1.Text = "計算";
 			this.button1.UseVisualStyleBackColor = true;
 			this.button1.Click += new System.EventHandler(this.button1_Click);
@@ -99,14 +109,14 @@
 			this.textBox1.Location = new System.Drawing.Point(64, 48);
 			this.textBox1.Name = "textBox1";
 			this.textBox1.Size = new System.Drawing.Size(100, 28);
-			this.textBox1.TabIndex = 1;
+			this.textBox1.TabIndex = 0;
 			// 
 			// label2
 			// 
 			this.label2.Location = new System.Drawing.Point(183, 54);
 			this.label2.Name = "label2";
 			this.label2.Size = new System.Drawing.Size(59, 24);
-			this.label2.TabIndex = 0;
+			this.label2.TabIndex = 6;
 			this.label2.Text = "性別：";
 			// 
 			// label1
@@ -115,7 +125,7 @@
 			this.label1.Location = new System.Drawing.Point(15, 51);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(57, 20);
-			this.label1.TabIndex = 0;
+			this.label1.TabIndex = 4;
 			this.label1.Text = "年齡：";
 			// 
 			// groupBox2
@@ -128,7 +138,7 @@
 			this.groupBox2.Location = new System.Drawing.Point(12, 205);
 			this.groupBox2.Name = "groupBox2";
 			this.groupBox2.Size = new System.Drawing.Size(398, 187);
-			this.groupBox2.TabIndex = 0;
+			this.groupBox2.TabIndex = 1;
 			this.groupBox2.TabStop = false;
 			this.groupBox2.Text = "計算結果";
 			// 
@@ -147,7 +157,7 @@
 			this.label5.Location = new System.Drawing.Point(136, 54);
 			this.label5.Name = "label5";
 			this.label5.Size = new System.Drawing.Size(30, 20);
-			this.label5.TabIndex = 2;
+			this.label5.TabIndex = 1;
 			this.label5.Text = "---";
 			// 
 			// label4
@@ -156,7 +166,7 @@
 			this.label4.Location = new System.Drawing.Point(35, 100);
 			this.label4.Name = "label4";
 			this.label4.Size = new System.Drawing.Size(57, 20);
-			this.label4.TabIndex = 1;
+			this.label4.TabIndex = 2;
 			this.label4.Text = "原因：";
 			// 
 			// label3
@@ -167,16 +177,6 @@
 			this.label3.Size = new System.Drawing.Size(57, 20);
 			this.label3.TabIndex = 0;
 			this.label3.Text = "車資：";
-			// 
-			// label7
-			// 
-			this.label7.AutoSize = true;
-			this.label7.Font = new System.Drawing.Font("Microsoft JhengHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-			this.label7.Location = new System.Drawing.Point(248, 77);
-			this.label7.Name = "label7";
-			this.label7.Size = new System.Drawing.Size(99, 20);
-			this.label7.TabIndex = 4;
-			this.label7.Text = "(預設為男性)";
 			// 
 			// Form1
 			// 
